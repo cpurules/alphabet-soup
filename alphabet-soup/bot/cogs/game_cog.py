dcb2e0143a79857e6c2cc49a927c4bdb25722018
Charlie Unfricht <dzcharlie@gmail.com>
2021-12-29 02:26:26 -0500

    Add stub handling for &start

--- alphabet-soup/bot/cogs/game_cog.py
+++ alphabet-soup/bot/cogs/game_cog.py
@@ -1,8 +1,13 @@
 import asyncio
+import core.context
+import core.dice
+import core.dictionary
+import core.game
+import core.player
 import discord
 
-from discord.ext import commands
 from bot.embed_builder import EmbedBuilder
+from discord.ext import commands
 
 class GameCog(commands.Cog):
     def __init__(self, bot):
@@ -11,17 +16,20 @@ class GameCog(commands.Cog):
     @commands.command(name='start')
     async def start_game(self, ctx):
         await ctx.send(content='You started a game!')
-        #response_embed = EmbedBuilder().set_title("Alphabet Soup - New Game")
-        #game_fields = {
-        #    '_key': ctx.message.id,
-        #    'owner': ctx.author.id,
-        #    'guild': GameGuild.create_from_ctx(ctx)
-        #}
-        #try:
-        #    new_game = Game.create_new(**game_fields)
-        #    await ctx.send(content='Created new game!')
-        #except Exception as e:
-        #    await ctx.send(content=str(e))
+        response_embed = EmbedBuilder().set_title("Alphabet Soup - New Game")
+        
+        game_fields = {
+            '_key': ctx.message.id,
+            'context': core.context.Context.create_from_ctx(ctx),
+            'players': [core.player.Player.create_from_id(ctx.author.id)],
+            'dictionary': core.dictionary.Dictionary.SOWPODS_ENGLISH,
+            'dice': core.dice.Dice.NEW_ENGLISH,
+            'time_limit': 180
+        }
+        try:
+            new_game = core.game.Game(**game_fields)
+        except Exception as e:
+            await ctx.send(content=str(e))
 
 def setup(bot):
     bot.add_cog(GameCog(bot))
