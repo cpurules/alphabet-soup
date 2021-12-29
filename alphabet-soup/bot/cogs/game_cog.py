377c108537062b5cf0ee2fd6679086ba17053586
Charlie Unfricht <dzcharlie@gmail.com>
2021-12-29 03:01:57 -0500

    Implement stub &start command and add check in &play for active game

--- alphabet-soup/bot/cogs/game_cog.py
+++ alphabet-soup/bot/cogs/game_cog.py
@@ -15,18 +15,23 @@ class GameCog(commands.Cog):
         self.bot = bot
     
     @commands.command(name='play')
-    async def start_game(self, ctx):
+    async def create_lobby(self, ctx):
         response_embed = EmbedBuilder().set_title("Alphabet Soup - New Game")
-        
-        game_fields = {
-            '_key': ctx.message.id,
-            'context': core.context.Context.create_from_ctx(ctx),
-            'players': [core.player.Player.create_from_id(ctx.author.id)],
-            'dictionary': core.dictionary.Dictionary.SOWPODS_ENGLISH,
-            'dice': core.dice.Dice.NEW_ENGLISH,
-            'time_limit': timedelta(minutes=3)
-        }
-        try:
+
+        game_context = core.context.Context.create_from_ctx(ctx)
+        if not core.game.ActiveGame.get_by_context(game_context) is None:
+            response_embed = response_embed.set_description("**Oops!**") \
+                                            .append_to_description("It looks like there's already an active game in this channel!") \
+                                            .append_to_description("You can type `&join` to join.")
+        else:
+            game_fields = {
+                '_key': ctx.message.id,
+                'context': core.context.Context.create_from_ctx(ctx),
+                'players': [core.player.Player.create_from_id(ctx.author.id)],
+                'dictionary': core.dictionary.Dictionary.SOWPODS_ENGLISH,
+                'dice': core.dice.Dice.NEW_ENGLISH,
+                'time_limit': timedelta(minutes=3)
+            }
             new_game = core.game.Game(**game_fields)
             response_embed = response_embed.set_description("You've created a new lobby for Alphabet Soup!") \
                                             .append_to_description("Below are the current game options:") \
@@ -38,9 +43,22 @@ class GameCog(commands.Cog):
                                             .append_to_description("") \
                                             .append_to_description("Use `&join` to join the game!") \
                                             .append_to_description("Hosts: use `&options` to update game options, or `&start` to begin the game")
-            await ctx.send(embed=response_embed.build())
-        except Exception as e:
-            await ctx.send(content=str(e))
+        
+        await ctx.send(embed=response_embed.build())
+    
+    @commands.command(name='start')
+    async def start_game(self, ctx):
+        response_embed = EmbedBuilder().set_title("Alphabet Soup - Start Game")
+
+        game_context = core.context.Context.create_from_ctx(ctx)
+        pending_game = core.game.Game.get_by_context(game_context, match_owner=True)
+        if pending_game is None:
+            response_embed = response_embed.set_description("**Oops!**") \
+                                            .append_to_description("It doesn't look like you own an active lobby in this channel!")
+        else:
+            response_embed = response_embed.set_description("OK cool")
+        
+        await ctx.send(embed=response_embed.build())
 
 def setup(bot):
     bot.add_cog(GameCog(bot))
